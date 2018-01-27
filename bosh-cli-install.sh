cbca7232a4a722629cfbdc5a5598edbcfd105e2d
Simon O'Brien <simon@the-obriens.net>
2018-01-27 14:37:19 +0000

    Initial commit

---
+++ bosh-cli-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+BOSH_HREF=https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.48-linux-amd64
+BOSH_CLI_NAME=bosh
+
+apt update
+apt-get upgrade -y
+apt install wget
+
+# download bosh
+wget -O $BOSH_CLI_NAME $BOSH_HREF
+
+# make executable
+chmod +x $BOSH_CLI_NAME
+
+# move to local bin
+mv $BOSH_CLI_NAME /usr/local/bin/$BOSH_CLI_NAME
+
+$BOSH_CLI_NAME -v
