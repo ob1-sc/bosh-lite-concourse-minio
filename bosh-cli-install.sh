49590e1de985e980191bbb3f7ede2afd7f7727bf
Simon O'Brien <simon@the-obriens.net>
2018-01-30 11:32:14 +0000

    Added required bosh cli dependencies to the install

--- bosh-cli-install.sh
+++ bosh-cli-install.sh
@@ -3,6 +3,10 @@
 BOSH_HREF=https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.48-linux-amd64
 BOSH_CLI_NAME=bosh
 
+# install bosh cli dependencies
+apt-get update
+apt-get install -y build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt-dev libxml2-dev libssl-dev libreadline6 libreadline6-dev libyaml-dev libsqlite3-dev sqlite3
+
 # download bosh
 wget -O $BOSH_CLI_NAME $BOSH_HREF
 
